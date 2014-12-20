779cef71f1a3749ea844e38c841c51a16f9cfee8
shartep <shartep@mail.ru>
2014-12-20 19:13:33 +0200

    para_3

---
+++ ClassWork/20.12.14/Iclonable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iclonable
+{
+    class Monster : ICloneable
+    {
+        string name;
+        int health, ammo;
+
+        public Monster(int health, int ammo, string name)
+        {
+            this.health = health;
+            this.ammo = ammo;
+            this.name = name;
+        }
+        public Monster ShallowClone() // поверхностная копия
+        {
+            return (Monster)this.MemberwiseClone();
+        }
+
+        public object Clone() // пользовательская копия
+        {
+            return new Monster(this.health, this.ammo, "Клон " + this.name);
+        }
+
+        virtual public void Passport()
+        {
+            Console.WriteLine("Monster {0} \t health = {1} ammo = {2}", name, health, ammo);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Monster Andrew = new Monster(70, 80, "Андрей");
+            Monster X = Andrew;
+            Monster Y = Andrew.ShallowClone();
+            Monster Z = (Monster)Andrew.Clone();
+        }
+    }
+}
---
+++ ClassWork/20.12.14/Icomarable/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icomarable
+{
+    class Monster : IComparable
+    {
+        string name;
+        int health;
+        int ammo;
+
+        public Monster(int health, int ammo, string name)
+        {
+            this.health = health;
+            this.ammo = ammo;
+            this.name = name;
+        }
+
+        virtual public void Passport()
+        {
+            Console.WriteLine("Monster {0} \t health = {1} ammo = {2}", name, health, ammo);
+        }
+
+        public int CompareTo(object obj) // реализация интерфейса
+        {
+            Monster temp = obj as Monster;
+            //if (this.ammo > temp.ammo) return 1;
+            //if (this.ammo < temp.ammo) return -1;
+            //return 0;
+            return name.CompareTo(temp.name);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Monster[] crowd = new Monster[3];
+
+            crowd[0] = new Monster(50, 50, "Кристина");
+            crowd[1] = new Monster(80, 80, "Александр");
+            crowd[2] = new Monster(40, 10, "Евгений");
+
+            Array.Sort(crowd); // сортировка стала возможной
+
+            foreach (Monster elem in crowd) elem.Passport();
+        }
+    }
+}
