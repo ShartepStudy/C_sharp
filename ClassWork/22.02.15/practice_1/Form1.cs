d54199f9c883ffb386dc26ceea156917e4db6114
shartep <shartep@mail.ru>
2015-04-04 14:52:50 +0300

    04-04-2

---
+++ ClassWork/22.02.15/practice_1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice_1
+{
+    public partial class Form1 : Form
+    {
+        private int _count = 2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_MouseEnter(object sender, EventArgs e)
+        {
+            var parent = (sender as ToolStripMenuItem);
+            if (parent != null && parent.DropDownItems.Count == 0)
+            {
+                var newItem = new ToolStripMenuItem
+                {
+                    Text = (_count++).ToString()
+                };
+                newItem.MouseEnter += new System.EventHandler(this.toolStripMenuItem1_MouseEnter);
+                parent.DropDownItems.Add(newItem);
+            }
+        }
+
+
+    }
+}
