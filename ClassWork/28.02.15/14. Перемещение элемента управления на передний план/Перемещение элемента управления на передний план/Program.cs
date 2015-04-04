0d5cacf2cf42fd503717f10e65a8afe2c0a78e14
shartep <shartep@mail.ru>
2015-04-04 15:19:46 +0300

    46

---
+++ ClassWork/28.02.15/14. Перемещение элемента управления на передний план/Перемещение элемента управления на передний план/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new CustomForm());
+        }
+    }
+}
