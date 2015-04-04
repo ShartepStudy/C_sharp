c51f45b7a3c1433e9339baed91ef1fbb4fc414cc
shartep <shartep@mail.ru>
2015-04-04 15:17:26 +0300

    9

---
+++ ClassWork/28.02.15/14. Перемещение элемента управления на передний план/Перемещение элемента управления на передний план/CustomForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    internal partial class CustomForm :
+        Form
+    {
+        public CustomForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(Object sender, EventArgs arguments)
+        {
+            Controls.SetChildIndex(sender as Button, 0);
+        }
+    }
+}
